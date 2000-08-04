606e2987db079d8cd0b3df85f22e6c5ee71f4478
tamnd <1218621+tamnd@users.noreply.github.com>
2000-08-04 22:44:37 +0000

    Add Python 1.6b1 sources
    Extracted from https://legacy.python.org/download/releases/src/, committed at
    the original tarball timestamp so the history timeline lines up with the
    real release dates.

---
+++ src/1.6b1/Lib/distutils/__init__.py
@@ -0,0 +1,13 @@
+"""distutils
+
+The main package for the Python Module Distribtion Utilities.  Normally
+used from a setup script as
+
+   from distutils.core import setup
+
+   setup (...)
+"""
+
+__revision__ = "$Id: __init__.py,v 1.8 2000/06/30 02:54:36 gward Exp $"
+
+__version__ = "0.9"
