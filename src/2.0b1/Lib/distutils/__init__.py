55c6e4de4ea0769dd46f3fe75a9b96950dd42c46
tamnd <1218621+tamnd@users.noreply.github.com>
2000-09-05 19:43:38 +0000

    Add Python 2.0b1 sources
    Extracted from https://legacy.python.org/download/releases/src/, committed at
    the original tarball timestamp so the history timeline lines up with the
    real release dates.

---
+++ src/2.0b1/Lib/distutils/__init__.py
@@ -0,0 +1,13 @@
+"""distutils
+
+The main package for the Python Module Distribtion Utilities.  Normally
+used from a setup script as
+
+   from distutils.core import setup
+
+   setup (...)
+"""
+
+__revision__ = "$Id: __init__.py,v 1.12 2000/09/01 01:00:40 gward Exp $"
+
+__version__ = "0.9.2"
